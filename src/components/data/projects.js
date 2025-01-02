07f0f6931d464ff5c830dcd62ce9bc33b367c715
Rakesh chourasia <ashlinsunil1@gmail.com>
2025-01-02 13:25:30 +0530

    initial commit

---
+++ src/components/data/projects.js
@@ -0,0 +1,43 @@
+const projects = {
+  1: {
+    key: 1,
+    title: "Netflix Data Analysis",
+    imageSrc:"new.png",
+    description: "This is a project made with Python, Excel, SQL and PowerBI.",
+    demo: "https://github.com/Rakeshchourasia/Netflix-Data-Analysis",
+    source: "https://github.com/Rakeshchourasia/Netflix-Data-Analysis"
+  },
+  2: {
+    key: 2,
+    title: "Zomato Sales Data Analysis",
+    imageSrc: "zomato.png",
+    description: "This is a project made with SQL and PowerBI",
+    demo: "https://github.com/Rakeshchourasia/Zomato_Sales_analysis_using_sql_and_powerBi",
+    source: "https://github.com/Rakeshchourasia/Zomato_Sales_analysis_using_sql_and_powerBi"
+  },
+  3: {
+    key: 3,
+    title: "Super Store Sales Analysis",
+    imageSrc: "superstore.png",
+    description: "This is a project made with SQL, Excel and PowerBI",
+    demo: "https://github.com/Rakeshchourasia/super-store-sales-analysis-using-sql-and-power-Bi",
+    source: "https://github.com/Rakeshchourasia/super-store-sales-analysis-using-sql-and-power-Bi"
+  },
+  4: {
+    key: 4,
+    title: "Stock Company Sales Analysis",
+    imageSrc: "stock.png",
+    description: "This is a project made with SQL, Excel and PowerBI",
+    demo: "https://github.com/Rakeshchourasia/Pizzza-Shop-Sales-Analysis",
+    source: "https://github.com/Rakeshchourasia/Pizzza-Shop-Sales-Analysis"
+  },
+  5: {
+    key: 5,
+    title: "Hospitality Domain Data Analysis",
+    imageSrc: "Hospitality.png",
+    description: "This is a project made with SQL, Excel and PowerBI",
+    demo: "https://github.com/Rakeshchourasia/Hospitality-Domain-Data-Analysis-with-powerBi",
+    source:"https://github.com/Rakeshchourasia/Hospitality-Domain-Data-Analysis-with-powerBi"
+  }
+};
+export default projects;
